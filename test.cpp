e799aed16f1ed1b73630e82b7b567cab690f18fa
Ömer Bartu <bartu0432@gmail.com>
2025-09-24 23:07:55 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,40 @@
 #include <vector>
 #include <queue>
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+int swap(int * num1, int * num2);
 
 int main(int argc, char const *argv[])
 {
+    
     int a = 5;
     int b = 3;
-    char c = 'c';
-    float d = 4.5;
+    char c = 'c' + 1;
+    float d = 123414.5123123;
     double e = 1.467e-30;
     float f = (float) e;
-    cout << e << endl; // Outputs 1.66667
-    cout << f << endl; // Outputs 1.66667
+    int k = (int) d;
 
+    // for (size_t i = 0; i < 20; i++)
+    // {
+    //     int random = rand();
+    //     cout << random << endl;
+    // }
+
+    cout <<  c;
+    
+
+    
+
+    return 0;
+}
+
+int swap(int * num1, int * num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
     return 0;
 }
