3b9239fa0b5d2087c8fcdabeff28daa72cfb10c0
Ömer Bartu <bartu0432@gmail.com>
2025-10-30 23:12:03 +0300

    SingleNode class definition

--- List/SingleNode.cpp
+++ List/SingleNode.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
 #include "SingleNode.h"
 
-
+SingleNode::SingleNode() : val(0), next(nullptr) {}
 SingleNode::SingleNode(int val) : val(val), next(nullptr) {}
+
+SingleNode::~SingleNode() {};
+
+void SingleNode::print() const {
+    std::cout << val << " ";
+}
+
+
+// Getter methods
+SingleNode* SingleNode::getNext() {
+    return next;
+}
+
+int SingleNode::getVal() {
+    return val;
+}
--- List/SingleNode.h
+++ List/SingleNode.h
@@ -7,12 +7,17 @@ public:
     SingleNode();
     SingleNode(int val);
     ~SingleNode();
+
+
+    void print() const;
+
+
+    // Getter methods
+    SingleNode* getNext();
+    int getVal();
+
+    // Setter methods
+    void setNext(SingleNode* next);
 };
 
-SingleNode::SingleNode()
-{
-}
 
-SingleNode::~SingleNode()
-{
-}
