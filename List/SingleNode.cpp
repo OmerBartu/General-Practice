b524e850e50916ba87d3b066c4263532c7ff73e2
Ömer Bartu <bartu0432@gmail.com>
2025-10-30 23:06:37 +0300

    Add constructor

---
+++ List/SingleNode.cpp
@@ -0,0 +1,4 @@
+#include "SingleNode.h"
+
+
+SingleNode::SingleNode(int val) : val(val), next(nullptr) {}
---
+++ List/SingleNode.h
@@ -0,0 +1,18 @@
+class SingleNode
+{
+private:
+    int val;
+    SingleNode *next;
+public:
+    SingleNode();
+    SingleNode(int val);
+    ~SingleNode();
+};
+
+SingleNode::SingleNode()
+{
+}
+
+SingleNode::~SingleNode()
+{
+}
